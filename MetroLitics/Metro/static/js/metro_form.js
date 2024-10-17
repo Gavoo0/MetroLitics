5358be4ebb352e34037047d9dab8e0e7f7717005
Plaza Vespucio <pvespucio@PVAWSB03LC0416.duoc.acad>
2024-10-17 18:33:31 -0300

    terminao

--- MetroLitics/Metro/static/js/metro_form.js
+++ MetroLitics/Metro/static/js/metro_form.js
@@ -3,9 +3,8 @@ $(document).ready(function(){
         linea = $("#linea").val();
         cantidad = $("#aglomeracion").val();
         fecha = $("#fecha").val();
-        linea2 = $("#linea2").val();
-        cantidad2 = $("#personas").val();
-        fecha2 = $("#fecha2").val();
+        cantidad2 = $("#personas_bus").val();
+        fecha2 = $("#fecha_bus").val();
         valid = true;
         if(linea === null){
             $("#linea").css("border-color","red");
@@ -34,31 +33,21 @@ $(document).ready(function(){
             $("#mensaje_error1").fadeOut();
         }
 
-
-        if(linea2 === null){
-            $("#linea2").css("border-color","red");
-            $("#mensaje_error3").fadeIn();
-            valid = false;
-        } else {
-            $("#linea2").css("border-color","#ced4da"); 
-            $("#mensaje_error3").fadeOut();
-        }
-
         if(cantidad2 === ""){
-            $("#personas").css("border-color","red");
+            $("#personas_bus").css("border-color","red");
             $("#mensaje_error5").fadeIn();
             valid = false;
         } else {
-            $("#personas").css("border-color","#ced4da");
+            $("#personas_bus").css("border-color","#ced4da");
             $("#mensaje_error5").fadeOut();
         }
 
         if(fecha2 == ""){
-            $("#fecha2").css("border-color","red");
+            $("#fecha_bus").css("border-color","red");
             $("#mensaje_error4").fadeIn();
             valid = false;
         }else{
-            $("#fecha2").css("border-color","#ced4da");
+            $("#fecha_bus").css("border-color","#ced4da");
             $("#mensaje_error4").fadeOut();
         }
 
@@ -101,40 +90,28 @@ $(document).ready(function(){
             $("#fecha").css("border-color","#ced4da");
             $("#mensaje_error1").fadeOut();
         }
-    })
+    })  
 
-    $("#linea2").blur(function() {
-        linea2 = $("#linea2").val();
-        if(linea2 === null){
-            $("#linea2").css("border-color","red");
-            $("#mensaje_error3").fadeIn();
-            valid = false;
-        } else {
-            $("#linea2").css("border-color","#ced4da"); 
-            $("#mensaje_error3").fadeOut();
-        }
-    });
-
-    $("#personas").blur(function() {
-        cantidad2 = $("#personas").val();
+    $("#personas_bus").blur(function() {
+        cantidad2 = $("#personas_bus").val();
         if(cantidad2 === ""){
-            $("#personas").css("border-color","red");
+            $("#personas_bus").css("border-color","red");
             $("#mensaje_error5").fadeIn();
             valid = false;
         } else {
-            $("#personas").css("border-color","#ced4da");
+            $("#personas_bus").css("border-color","#ced4da");
             $("#mensaje_error5").fadeOut();
         }
     });
 
-    $("#fecha2").blur(function() {
-        fecha2 = $("#fecha2").val();
+    $("#fecha_bus").blur(function() {
+        fecha2 = $("#fecha_bus").val();
         if(fecha2 == ""){
-            $("#fecha2").css("border-color","red");
+            $("#fecha_bus").css("border-color","red");
             $("#mensaje_error4").fadeIn();
             valid = false;
         }else{
-            $("#fecha2").css("border-color","#ced4da");
+            $("#fecha_bus").css("border-color","#ced4da");
             $("#mensaje_error4").fadeOut();
         }
     });
--- MetroLitics/Metro/views.py
+++ MetroLitics/Metro/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from .models import Mantenedor_metro
+from .models import Mantenedor_metro, Mantenedor_bus, Reporte
 from datetime import datetime, timedelta
 # Create your views here.
 def index(request):
@@ -20,5 +20,57 @@ def analiticas(request):
     return render(request, 'Metro/analiticas.html',context)
 
 def reportes(request):
+    if request.method == "POST":
+        linea_metro = request.POST.get('linea')
+        fecha_metro = request.POST.get('fecha')
+        aglomeracion = int(request.POST.get('aglomeracion'))
+        fecha_bus = request.POST.get('fecha_bus')
+        personas_bus = int(request.POST.get('personas_bus'))
+        
+
+        metros = Mantenedor_metro.objects.filter(fecha=fecha_metro, linea_metro=linea_metro)
+
+        if metros.exists():
+            # Si ya existen registros, actualizamos el primero
+            metro = metros.first()
+            metro.aglomeracion = (aglomeracion + metro.aglomeracion)-personas_bus
+            metro.save()
+        else:
+            # Si no existe, creamos un nuevo registro
+            print("hola")
+            metro = Mantenedor_metro(
+                linea_metro=linea_metro,
+                fecha=fecha_metro,
+                aglomeracion=aglomeracion-personas_bus
+            )
+            metro.save()
+        
+        bus = Mantenedor_bus.objects.filter(fecha=fecha_bus)
+
+        buses = Mantenedor_bus.objects.filter(fecha=fecha_bus)
+
+        if buses.exists():
+            # Si ya existen registros, actualizamos el primero
+            bus = buses.first()
+            bus.f_id_metro = metro
+            bus.cantidad_personas = personas_bus
+            bus.save()
+        else:
+            # Si no existe, lo creamos
+            bus = Mantenedor_bus(
+                f_id_metro=metro,
+                fecha=fecha_bus,
+                cantidad_personas=personas_bus
+            )
+            bus.save()
+        
+        nuevo_reporte = Reporte(
+            f_id_metro = metro,
+            f_id_bus = bus,
+            fecha = fecha_metro
+        )
+        
+        nuevo_reporte.save()
+        
     context={}
     return render(request, 'Metro/reportes.html',context)
